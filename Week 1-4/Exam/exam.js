78510b42bfaffd1d33d7db68e08f6983f468827f
James Bucsko <notjamesbucsko@gmail.com>
2022-03-01 23:16:14 -0800

    week 1-4

---
+++ Week 1-4/Exam/exam.js
@@ -0,0 +1,12 @@
+var element = document.querySelector(".cookiebox")
+
+function remove(element) {
+    element.remove();
+}
+
+function newimg(element) {
+    document.getElementById("succulent1").src="images/succulents-2.jpg";
+}
+function oldimg(element){
+    document.getElementById("succulent1").src="images/succulents-1.jpg"
+}
